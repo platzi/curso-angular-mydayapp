0541a67179fd5fa0ffdea73861e4238722088daf
Nicolas Molina <hi@nicobytes.com>
2023-09-25 11:59:59 -0500

    save

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -26,4 +26,17 @@ export class LabsComponent {
     age: 18,
     avatar: 'https://w3schools.com/howto/img_avatar.png'
   }
+
+  clickHandler() {
+    alert('Hola')
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }

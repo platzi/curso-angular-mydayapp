9f4e21a5412a5f71c0d14035b24b091ba460d741
Nicolas Molina <hi@nicobytes.com>
2023-09-26 11:23:48 -0500

    save

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -2,4 +2,5 @@ export interface Task {
     id: number;
     title: string;
     completed: boolean;
+    editing?: boolean;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -81,4 +81,37 @@ export class HomeComponent {
     })
   }
 
+  updateTaskEditingMode(index: number) {
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          ...task,
+          editing: false
+        };
+      })
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing: false
+          }
+        }
+        return task;
+      })
+    });
+  }
+
 }

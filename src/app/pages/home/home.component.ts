ecab3aed4f3a47fa21b340ed2ab58e758a8f8ffd
Nicolas Molina <hi@nicobytes.com>
2023-11-04 12:20:27 -0500

    Fix save

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,4 +15,14 @@ export class HomeComponent {
     'Crear componente',
     'Crear  servicio',
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
 }
